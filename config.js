db5b3fcff937580f68791fbe7cae125fefa27aab
falook555 <falook555@gmail.com>
2023-01-18 14:33:51 +0700

    update180166

--- config.js
+++ config.js
@@ -1,8 +1,6 @@
 const config = {
-    // api: 'https://api-queue-ss.diligentsoftinter.com'
-    // api: 'http://110.49.126.23:4059'
-    api: 'http://localhost:4059'
-    // api: 'https://sw-center-api.diligentsoftinter.com'
+    api: 'http://110.49.126.23:4059'
+    // api: 'http://localhost:4059'
 }
 
 
